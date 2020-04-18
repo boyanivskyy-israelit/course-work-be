d4b472b0d0a4bf9657096d4f496b2c6ddbdb6333
Vitaliy Boyanivskyy <vitaliy.boyanivskiy@israelit.pro>
2020-04-18 14:29:28 +0300

    Some changes to fix heroku deploy

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const routes = require('./routers');
 require('dotenv').config();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 require('./db/db');
 
 const app = express();
@@ -11,12 +11,10 @@ const app = express();
 // middlewares
 app.use(express.json());
 app.use(morgan('dev'));
-app.use(helmet());
+// app.use(helmet());
 
 app.use('/api/', routes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
-// git push heroku master
--- routers/onboarding.js
+++ routers/onboarding.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const User = require('../models/User');
-const auth = require('../middleware/auth');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: 'Hello from heroku on route /api/onboarding',
+  });
+});
+
+router.get('/questions', (req, res) => {
   res.status(200).json({
     success: true,
     message: 'Hello there!',
