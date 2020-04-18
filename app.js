e5b29d8fd72c5ec3dfaf0697b25f4ff33e1afdf5
Vitaliy Boyanivskyy <vitaliy.boyanivskiy@israelit.pro>
2020-04-18 14:40:05 +0300

    Add CORS middleware

--- app.js
+++ app.js
@@ -4,9 +4,17 @@ const morgan = require('morgan');
 const routes = require('./routers');
 require('dotenv').config();
 const port = process.env.PORT || 3000;
-require('./db/db');
 
 const app = express();
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+require('./db/db');
 
 // middlewares
 app.use(express.json());
